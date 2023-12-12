337685e3ff10e00699a8f235e03d9740d799a873
koeunyeon <koeunyeon@gmail.com>
2023-12-12 15:25:21 +0900

    모델은 당분간 사용하지 않을 예정이라서 삭제

--- server/model/membermodel.py
+++
@@ -1,8 +0,0 @@
-from keyfa.model.basemodel import BaseModel
-from keyfa.util.modelutil import column_by_rule
-
-class MemberModel(BaseModel):
-    email = column_by_rule("email", nullable=False)
-    email_key = column_by_rule("email_key")
-    expired_at = column_by_rule("expired_at")
-    status = column_by_rule("status", default='email_not_confirmed', nullable=False)
