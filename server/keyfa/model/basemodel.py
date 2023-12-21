bd9de0c549c5a03e0aaeac9f28cd9277591d29c6
koeunyeon <koeunyeon@gmail.com>
2023-12-21 11:15:46 +0900

    rdb basemodel 네임스페이스 및 클래스 변경 keyfa.model.basemodel.BaseModel => keyfa.rdb.baseentity.BaseEntity

--- server/keyfa/model/basemodel.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column, DateTime, Integer, func, CHAR
-
-class BaseModel:
-    id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime, nullable=False, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-    use_yn = Column(CHAR(1), nullable=False, default='Y')
