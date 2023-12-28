418b7e143c40ba526fed5181bcfb42036865843c
koeunyeon <koeunyeon@gmail.com>
2023-12-28 10:44:38 +0900

    baseEntity 정의 entityutil 기반으로 변경

--- server/keyfa/rdb/baseentity.py
+++ server/keyfa/rdb/baseentity.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, DateTime, Integer, func, CHAR
+from keyfa.util.entityutil import column
 
 class BaseEntity:
-    id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime, nullable=False, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-    use_yn = Column(CHAR(1), nullable=False, default='Y')
+    id = column("id") # same as Column(Integer, primary_key=True, index=True)
+    created_at = column("created_at") # same as Column(DateTime, nullable=False, default=func.now())
+    updated_at = column("updated_at") # same as Column(DateTime, default=func.now(), onupdate=func.now())
+    use_yn = column("use_yn") # same as Column(CHAR(1), nullable=False, default='Y')
+    
